97620d2280f13a6aac63758cc48dcf95e1e71162
Barry Shovlin <bshovlin32@gmail.com>
2021-02-26 15:31:08 -0600

    completed translations from C# to javascript

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,28 @@
-// Put your code here
+const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+console.log("All Place Names");
+
+const nameLoop = (namesArray) => {
+    for (const name of namesArray) {
+        console.log(name)
+    }
+}
+
+nameLoop(names)
+
+console.log(" ")
+
+let newArray = []
+
+const nameFilter = () => {
+    console.log("'The' Place Names")
+    names.filter(name => {
+        if (name.includes("The")) {
+            newArray.push(name)
+        }
+    })
+    console.log(newArray)
+}
+
+nameFilter()
