6824630b07e596e4b52823c7114b729324e4c30f
Barry Shovlin <bshovlin32@gmail.com>
2021-02-26 15:54:12 -0600

    started translations, not finished yet

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,25 @@
-// Put your code here
+console.log("My Enemy List!");
+console.log("----------------")
+
+const enemies = [
+    {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        Offenses: ["Being a jerk to me in elementary school", "Not being nice to me in elementary school"],
+        isReallyHated = true
+    },
+    {
+        firstName: "Darth",
+        lastName: "Vader",
+        Offenses: ["Cut off Luke's hand", "Murdered all thos kids", "Unkind management practices"],
+        isReallyHated = false
+    },
+    {
+        firstName: Betty",
+        lastName: "Rudelady",
+        Offenses: ["Cut off Luke's hand", "Murdered all thos kids", "Unkind management practices"],
+        isReallyHated = false
+    },
+
+
+]
