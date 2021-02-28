fa08d975008981ef7242ac627e1c5b5456e07b46
Barry Shovlin <bshovlin32@gmail.com>
2021-02-27 23:09:36 -0600

    completed the translations

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -6,20 +6,41 @@ const enemies = [
         firstName: "Joshua",
         lastName: "Flowers",
         Offenses: ["Being a jerk to me in elementary school", "Not being nice to me in elementary school"],
-        isReallyHated = true
+        isReallyHated: true
     },
     {
         firstName: "Darth",
         lastName: "Vader",
         Offenses: ["Cut off Luke's hand", "Murdered all thos kids", "Unkind management practices"],
-        isReallyHated = false
+        isReallyHated: false
     },
     {
-        firstName: Betty",
+        firstName: "Betty",
         lastName: "Rudelady",
         Offenses: ["Cut off Luke's hand", "Murdered all thos kids", "Unkind management practices"],
-        isReallyHated = false
+        isReallyHated: true
+    },
+    {
+        firstName: "Leon",
+        lastName: "Peck",
+        Offenses: ["Keeps giving me a hotplate"],
+        isReallyHated: false
     },
 
 
 ]
+
+const enemyList = (enemies) => {
+    for (const enemy of enemies) {
+        if (enemy.isReallyHated === true) {
+            console.log(`${enemy.firstName} ${enemy.lastName} (Really, really dislike!)`)
+        }
+        else {
+            console.log(`${enemy.firstName} ${enemy.lastName}`)
+        }
+    }
+}
+
+enemyList(enemies)
+
+
