4c9510934c7074b15ab26db630a871ec9cd33b2a
Barry Shovlin <bshovlin32@gmail.com>
2021-03-01 11:29:14 -0600

    got dice to roll random numbers and add them together.  Doubles will show the doubles message

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,33 @@
-// Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+const Die = (number) => {
+    return {
+        value: number
+    }
+}
+
+const Roll = () => {
+    const randoNumber = (min, max) => {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min + 1))
+    }
+    const dieValue = randoNumber(1, 6)
+    const die = Die(dieValue)
+    return die
+}
+
+for (let i = 0; i < 10; i++) {
+    die1 = Roll()
+    die2 = Roll();
+
+
+    let message = `${die1.value} + ${die2.value} === ${die1.value + die2.value}`
+    if (die1.value === die2.value) {
+        message += " DOUBLES!"
+    }
+
+    console.log(message)
+}
